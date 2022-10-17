26528a6411c3806bc45cfe02cd1392dd3f8b529b
sumingit <oksumin94@gmail.com>
2022-10-17 13:07:52 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 /**
  * 페이지 스크롤에 따른 요소 제어
  */
@@ -152,6 +135,3 @@ spyEls.forEach(function (spyEl){
     .addTo(new ScrollMagic.Controller());
 });
 
-// textContent = 값을 알아내거나 지정하는것
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
